50a8dceaa8e0089f9b046379a767085e570724a3
Tomáš Kubala <kubala.tomas@hotmail.com>
2016-05-12 22:11:26 +0200

    First upload

---
+++ js/fancybox/chngimg.js
@@ -0,0 +1,28 @@
+		// var $imgs = $("#testimg").find("img"),
+		//     i = 0;
+
+		// function changeImage(){
+		//     var next = (++i % $imgs.length);
+		//     $($imgs.get(next - 1)).fadeOut(500);
+		//     $($imgs.get(next)).fadeIn(500);
+		// }
+		// var interval = setInterval(changeImage, 2000);
+		    i = 0;
+
+		// function changeImage(){
+		//     var next = (++i % 4);
+		//     if (next == 0) {
+		//     	$('#v4').fadeOut(500);
+		//     	$('#v1').fadeIn(500);
+		//     } else if (next == 1) {
+		//     	$('#v1').fadeOut(500);
+		//     	$('#v2').fadeIn(500);
+		//     } else if (next == 2) {
+		//     	$('#v2').fadeOut(500);
+		//     	$('#v3').fadeIn(500);
+		//     } else if (next == 3) {
+		//     	$('#v3').fadeOut(500);
+		//     	$('#v4').fadeIn(500);
+		//     }    
+		// }
+		// setInterval(changeImage, 2000);
